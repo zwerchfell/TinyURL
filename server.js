868c67392029d8ea63149b7906656c4cea9d9c39
zzou36 <zzou36@gatech.edu>
2016-08-14 22:51:14 -0400

    implementing web page

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var restRouter = require('./routes/rest');
 var redirectRouter = require('./routes/redirect');
+var index = require('./routes/index');
 
 app.longToShortHash = {};
 app.shortToLongHash = {};
 
+app.use('/', index);
 app.use('/api/v1', restRouter);
 app.use('/:shortUrl', redirectRouter);
 
